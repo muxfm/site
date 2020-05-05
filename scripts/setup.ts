1584adbe0ccd728555c15651061b299bcaea0631
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-05 18:14:08 +0530

    :heavy_plus_sign: Add got for HTTP requests

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,1 +1,13 @@
+import { prompt } from "inquirer";
 
+const setup = async () => {
+  const { feedUrl }: { feedUrl: string } = await prompt([
+    {
+      name: "feedUrl",
+      message: "Feed URL",
+    },
+  ]);
+  console.log(answers);
+};
+
+setup();

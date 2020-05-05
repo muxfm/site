29c342347ce4090b106a84e8b4006481c2c778b6
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-05 18:23:48 +0530

    :construction: Work on setup script

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,13 +1,37 @@
 import { prompt } from "inquirer";
+import { promises as fs } from "fs";
+import { join } from "path";
+import got from "got";
+
+interface Feed {
+  "itunes:category": string[];
+  image: string;
+  author: string;
+  lastBuildDate: string;
+  title: string;
+  link: string;
+  copyright: string;
+  generator: string;
+  language: string;
+  description: string;
+}
 
 const setup = async () => {
-  const { feedUrl }: { feedUrl: string } = await prompt([
+  const { metaUrl }: { metaUrl: string } = await prompt([
     {
-      name: "feedUrl",
-      message: "Feed URL",
+      name: "metaUrl",
+      message: "Meta JSON URL",
     },
   ]);
-  console.log(answers);
+  console.log("Fetching...");
+  const { body }: { body: Feed } = await got.get(metaUrl, {
+    responseType: "json",
+  });
+  if (!body.title || !body.author) throw new Error("Unable to parse JSON URL");
+  const pkg: any = JSON.parse(
+    (await fs.readFile(join(".", "package.json"))).toString()
+  );
+  await fs.writeFile(join(".", "package-example.json"), pkg);
 };
 
 setup();
